871c5387c2968057ecaef4cf32fec1943a45d953
Marcus M. Darden <ontoral@gmail.com>
2013-01-02 13:18:41 -0500

    Extended basic ball operations and some minor refactoring.

--- Ball.py
+++ Ball.py
@@ -1,32 +1,66 @@
+import random
+import pygame
+
+
 class Ball(object):
     """A Ball class that is aware of pygame.
-    
+
     A small round ball to play Breakout.
     Coordinates are the center of the ball.
     """
-    def __init__(self, x, y, radius=8):
+    def __init__(self, screen_size, radius=8):
         """Create a Ball object.
-        
+
         Args:
-            x: an int, the initial x coordinate of the ball
-            y: an int, the initial y coordinate of the ball
+            screen_size: an int 2-tuple, of screen width and height
             radius: an optional int, the radius of the ball
         """
-        self.x, self.y = x, y
+        # Creation parameters
+        self.screen_width, self.screen_height = screen_size
         self.radius = radius
-        self.x_velocity, self.y_velocity = 0, -5
-        self.color = (255, 255, 64)
 
-    def update(self):
-        """Update the position of the ball."""
-        self.x += self.x_velocity
-        self.y += self.y_velocity
+        # Initial position and velocity
+        self.x, self.y = 0, 0
+        self.x_velocity, self.y_velocity = 0, 0
+        self.moving = False
 
-    def draw(self, pygame, screen):
+        self.color = 255, 255, 64
+
+    def draw(self, screen):
         """Draw the ball on the screen.
-        
+
         Args:
-            pygame: the active pygame object
             screen: the active screen object
         """
         pygame.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
+    def reset(self, paddle):
+        """Prepare the ball for a new round.
+
+        Attach the ball to the center of the paddle, and give it a random
+        upwards velocity vector.
+
+        Args:
+            paddle: the game's paddle object
+        """
+        self.x_velocity = random.randint(-3, 3)
+        self.y_velocity = -5
+        self.x = paddle.x
+        self.y = paddle.y - (self.radius * 2)
+        self.moving = False
+
+    def serve(self):
+        """Set the ball in motion."""
+        self.moving = True
+
+    def update(self, paddle):
+        """Update the position of the ball.
+
+        Args:
+            paddle: the game's paddle object
+        """
+        if self.moving:
+            self.x += self.x_velocity
+            self.y += self.y_velocity
+        else:
+            self.x = paddle.x
