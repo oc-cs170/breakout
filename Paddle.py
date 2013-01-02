b1001f0fe57204c13884b00994249f097c74ff8f
Marcus M. Darden <ontoral@gmail.com>
2013-01-02 13:19:19 -0500

    Some cleanup and minor refactoring.

--- Paddle.py
+++ Paddle.py
@@ -1,34 +1,55 @@
+import pygame
+
+
 class Paddle(object):
     """A Paddle class that is aware of pygame.
-    
+
     A small rectangular paddle to play Breakout.
-    Coordinates are the center of the paddle.
+    Coordinates are the center of the top edge of the paddle.
     """
-    def __init__(self, x, y, width, height):
+    def __init__(self, screen_size):
         """Create a Paddle object.
-        
+
         Args:
-            x: an int, the initial x coordinate of the paddle
-            y: an int, the initial y coordinate of the paddle
-            width: an int, the initial width of the paddle
-            height: an int, the initial height of the paddle
+            screen_size: an int 2-tuple of screen width and height
         """
-        self.x, self.y = x, y
-        self.width, self.height = width, height
-        self.rect = ((x + width) / 2, (y + height) / 2, width, height)
+        # Creation parameter
+        self.screen_width, self.screen_height = screen_size
+
+        # Size and location
+        self.width, self.height = 80, 16
+        self.x, self.y = self.screen_width / 2, self.screen_height - (2 * self.height)
+        self.rect = ((self.screen_width - self.width) / 2, self.y, self.width, self.height)
+
+        # Velocity
         self.x_velocity, self.y_velocity = 0, 0
 
+        self.color = 192, 192, 192
+
+    def draw(self, screen):
+        """Draw the paddle on the screen.
+
+        Args:
+            screen: the active screen object
+        """
+        pygame.draw.rect(screen, self.color, self.rect)
+
+    def reset(self):
+        """Prepare the paddle for a new round.
+
+        Does nothing...
+
+        Args:
+            none so far...
+        """
+        pass
+
     def update(self):
-        """Update the position of the paddle."""
+        """Update the position of the paddle.
+
+        Should be called every frame, by the main game loop to allow the
+        paddle to move.
+        """
         self.x += self.x_velocity
         self.y += self.y_velocity
         self.rect = (self.x - self.width / 2, self.y - self.height / 2, self.width, self.height)
-
-    def draw(self, pygame, screen):
-        """Draw the paddle on the screen.
-        
-        Args:
-            pygame: the active pygame object
-            screen: the active screen object
-        """
-        pygame.draw.rect(screen, (192, 192, 192), self.rect)
