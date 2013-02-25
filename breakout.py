d9d8dce0ac93216cfc9bb9eaa872793eb4b82b2f
Marcus M. Darden <marcus.darden@cs.olivetcollege.edu>
2013-02-25 14:18:19 -0500

    Game splash screen added.

--- breakout.py
+++ breakout.py
@@ -10,12 +10,17 @@ from ball import Ball
 from paddle import Paddle
 
 
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
+BORDER = 20
+
+
 class Breakout(object):
     def __init__(self):
         # Initilaize pygame and the display/window
         pygame.mixer.pre_init(44100)
         pygame.init()
-        self.screen_width, self.screen_height = 600, 800
+        self.screen_width, self.screen_height = SCREEN_WIDTH, SCREEN_HEIGHT
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))  # , pygame.FULLSCREEN)
         self.background = self.screen.copy()
         pygame.display.set_caption('Breakout')
@@ -36,6 +41,9 @@ class Breakout(object):
         self.game_over = False
         self.round = 0
 
+        # Clear the screen
+        self.screen.blit(self.background, (0, 0))
+
         self.new_round()
 
     def new_round(self):
@@ -48,6 +56,58 @@ class Breakout(object):
         self.paddle.reset()
         self.ball.reset(self.paddle)
 
+    def splash_screen(self):
+        title = 'BREAKOUT'
+        bg = (216, 216, 255)
+        fg = (0, 128, 0)
+        clock = pygame.time.Clock()
+
+        # Build the splash screen
+        splash = self.screen.copy()
+        splash.fill((0, 0, 0))
+        inner = (BORDER, BORDER, SCREEN_WIDTH - 2 * BORDER, SCREEN_HEIGHT - 2 * BORDER)
+        splash.fill(bg, inner)
+
+        # hide_screen = pygame.time.get_ticks()
+        font1 = pygame.font.SysFont('Arial', 80, bold=True)
+        antialias = True
+        width, height = font1.size(title)
+        x = (SCREEN_WIDTH - width) / 2
+        y = 2 * BORDER
+
+        for i in range(len(title)):
+            clock.tick(4)
+            self.screen.blit(splash, (0, 0))
+            surf = font1.render(title[0:i + 1], antialias, fg, bg)
+            self.screen.blit(surf, (x, y))
+            pygame.display.flip()
+
+        # clock.tick(1)
+        font2 = pygame.font.SysFont('Arial', 24, bold=True)
+        x *= 2
+        y = y + height + BORDER
+        lines = ['<-, ->: Move paddle',
+                 'Space: Serve ball',
+                 'Esc, Q: Quit game',
+                 ' ',
+                 ' ',
+                 'Press any key to start...']
+        for line in lines:
+            clock.tick(10)
+            surf = font2.render(line, antialias, fg, bg)
+            self.screen.blit(surf, (x, y))
+            y += surf.get_height()
+            pygame.display.flip()
+
+        waiting = True
+        while waiting:           # Pause loop
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    waiting = False
+                    if event.key == pygame.K_q:
+                        self.game_over = True
+                    break
+
     def play(self):
         """Start Breakout program.
 
@@ -55,11 +115,12 @@ class Breakout(object):
         The game loop checks for events, updates all objects, and then
         draws all the objects.
         """
+        self.splash_screen()
         self.new_game()
         while not self.game_over:           # Game loop
             self.clock.tick(50)             # Frame rate control
             for event in pygame.event.get():
-                if event.type == pygame.QUIT or event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.QUIT:
                     self.game_over = True
                     break
                 if event.type == pygame.KEYDOWN:
@@ -69,6 +130,9 @@ class Breakout(object):
                         self.paddle.velocity = -4
                     elif event.key == pygame.K_RIGHT:
                         self.paddle.velocity = 4
+
+                    if event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+                        self.game_over = True
 
                     # This starts a new round, it's only here for debugging purposes
                     if event.key == pygame.K_r:
